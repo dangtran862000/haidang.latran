7afffaa2ee3ca7bf218205cae4bdc453a60aef8e
Đăng La Trần Hải <s3836605@rmit.edu.vn>
2020-07-22 19:59:28 +0700

    abc

--- Exercise Python/Week 4 EX5.py
+++ Exercise Python/Week 4 EX5.py
@@ -1,2 +1,10 @@
 n=int(input("Triangular number: "))
 
+def print_triangular_numbers(n):
+    Sum=0
+    for i in range(1,n+1):
+        Sum=Sum+i
+        print(i, end="\t")
+        print(Sum)
+
+print_triangular_numbers(n)
