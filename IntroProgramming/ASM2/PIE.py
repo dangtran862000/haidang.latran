5dc5d6289ed4465a19b6586008f0f07840d88b4a
hai dang <dangtran862000@gmail.com>
2020-07-27 22:30:09 +0700

    abc

--- IntroProgramming/ASM2/PIE.py
+++ IntroProgramming/ASM2/PIE.py
@@ -1,45 +1,37 @@
 import turtle
+import random
 pieChart = turtle.Turtle()
 
 window = turtle.Screen()
 # creating an empty list
 listNumber = []
-listPhanTram=[]
 listAngle=[]
 
 n = int(input("Enter number of elements : "))
 sum=0
 
-
 for i in range(0, n):
     element = float(input())
     sum = sum + element
     listNumber.append(element)  # adding the element
-a=float()
 
-for a in listNumber:
-    phanTram=round(((a/sum)*100),1)
-    print(phanTram)
-    listPhanTram.append(phanTram)
-
-for b in listPhanTram:
-    angle=round(((b*360)/100),1)
-    print(angle)
+for i in listNumber:
+    phanTram=round(((i/sum)*100),1)
+    angle = round(((phanTram * 360) / 100), 1)
     listAngle.append(angle)
+
+color=["red","green","blue","pink","purple","yellow","darkred","darkblue","darkgreen"]
 
 def draw_piechart(angle):
     pieChart.begin_fill()
-    pieChart.fillcolor("red")
-    pieChart.right(angle)
-    pieChart.forward(200)
-
-    pieChart.backward(200)
+    pieChart.fillcolor(random.choice(color))
+    pieChart.circle(-200, angle)
+    pieChart.goto(0, 0)
     pieChart.end_fill()
-
-pieChart.goto(0, 200)
-pieChart.circle(-200, 360)
-pieChart.goto(0, 0)
-pieChart.left(90)
+    pieChart.left(90)
+    pieChart.forward(200)
+    pieChart.right(90)
+pieChart.goto(0,200)
 for c in listAngle:
     draw_piechart(c)
 
--- IntroProgramming/ASM2/check.py
+++ IntroProgramming/ASM2/check.py
@@ -1,21 +1,63 @@
 import turtle
+import random
 pieChart = turtle.Turtle()
 
 window = turtle.Screen()
+# creating an empty list
+listNumber = []
+listAngle=[]
+listPhanTram=[]
+n = int(input("Enter number of elements : "))
+sum=0
+
+for i in range(0, n):
+    element = float(input())
+    sum = sum + element
+    listNumber.append(element)  # adding the element
+
+for i in listNumber:
+    phanTram=round(((i/sum)*100),1)
+    listPhanTram.append(phanTram)
+
+for i in listPhanTram:
+    angle = round(((i * 360) / 100), 1)
+    listAngle.append(angle)
+
+color=["red","green","blue","pink","purple","yellow","darkred","darkblue","darkgreen"]
+
+def write_num(a):
+    pieChart.up()
+    pieChart.goto(0, 0)
+    pieChart.left(90)
+    pieChart.right(angle/2)
+    pieChart.forward(100)
+    pieChart.down()
+    pieChart.write(a)
+    pieChart.up()
+    pieChart.backward(100)
+    pieChart.left(90)
+    pieChart.forward(200)
+    pieChart.right(90)
+    pieChart.down()
+
+pieChart.goto(0,200)
+def draw_piechart(angle):
+    pieChart.begin_fill()
+    pieChart.fillcolor(random.choice(color))
+    pieChart.circle(-200, angle)
+    pieChart.goto(0, 0)
+    pieChart.end_fill()
+    pieChart.left(90)
+    pieChart.forward(200)
+    pieChart.right(90)
 
 
 
-pieChart.right(90)
-pieChart.goto(0,0)
-pieChart.left(180)
-pieChart.left(90-30)
-pieChart.forward(200)
-pieChart.left(90)
-pieChart.circle(200,60)
-pieChart.goto(0,0)
-pieChart.right(90)
-pieChart.forward(200)
-pieChart.left(90)
-pieChart.circle(200,45)
+
+for c in listAngle:
+    draw_piechart(c)
+
+for a in listPhanTram:
+    write_num(a)
 
 window.exitonclick()
