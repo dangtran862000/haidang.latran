060a0a469b803bbd0285cea5796120ba74d3ef16
latranhaidang <s3836605@rmit.edu.vn>
2020-08-19 09:38:12 +0700

    new

--- Exercise Python/Week 6 EX8.py
+++ Exercise Python/Week 6 EX8.py
@@ -1,9 +1,9 @@
 message = str(input("Input message to encrypt: "))
 
 alphabet_encrypt = "QTGABCDEFHIJKLMNOPRSUVXYZ"
+alphabet_decrypt = "QTGABCDEFHIJKLMNOPRSUVXYZ"
 
-
-def subtitution_cipher(message, alphabet_encrypt):
+def encrypt_message(message, alphabet_encrypt):
     message = message.upper()
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     position = 0
@@ -17,4 +17,19 @@ def subtitution_cipher(message, alphabet_encrypt):
     return message
 
 
-print(subtitution_cipher(message,alphabet_encrypt))
+def decrypt_message(message, alphabet_decrypt):
+    message = message.upper()
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    position = 0
+    for i in message:
+        if i == " ":
+            message = message.replace(i, " ")
+            position = position + 1
+        else:
+            message = message.replace(message[position], alphabet[alphabet.find(i)])
+            position = position + 1
+    return message
+
+
+print(encrypt_message(message, alphabet_encrypt))
+print(decrypt_message(message,alphabet_decrypt))
